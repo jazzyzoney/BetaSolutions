c35d788af168ab9f782886a0de167af641a3518b
jazzyzoney <jaqtash22@gmail.com>
2024-11-29 09:28:59 +0100

    employee model

--- src/main/java/org/example/betasolutions/employee/Employee.java
+++ src/main/java/org/example/betasolutions/employee/Employee.java
@@ -1,4 +1,54 @@
 package org.example.betasolutions.employee;
 
 public class Employee {
+    int employeeID;
+    String employeeName;
+    String employeeOffice;
+    String employeeProficiency;
+    String employeeSalary;
+
+    //getters
+    public int getEmployeeID() {
+        return employeeID;
+    }
+
+    public String getEmployeeName() {
+        return employeeName;
+    }
+
+    public String getEmployeeOffice() {
+        return employeeOffice;
+    }
+
+    public String getEmployeeProficiency() {
+        return employeeProficiency;
+    }
+
+    public String getEmployeeSalary() {
+        return employeeSalary;
+    }
+
+    //setters
+    public void setEmployeeID(int employeeID) {
+        this.employeeID = employeeID;
+    }
+
+    public void setEmployeeName(String employeeName) {
+        this.employeeName = employeeName;
+    }
+
+    public void setEmployeeOffice(String employeeOffice) {
+        this.employeeOffice = employeeOffice;
+    }
+
+    public void setEmployeeProficiency(String employeeProficiency) {
+        this.employeeProficiency = employeeProficiency;
+    }
+
+    public void setEmployeeSalary(String employeeSalary) {
+        this.employeeSalary = employeeSalary;
+    }
+
+    //constructor when needed
+
 }

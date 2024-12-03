8e20763bbcda58526fa566895f0cbb3e0c747c99
carobraaa <166133374+carobraaa@users.noreply.github.com>
2024-12-03 22:12:23 +0100

    Script med sample data

--- src/main/java/org/example/betasolutions/login/Login.java
+++ src/main/java/org/example/betasolutions/login/Login.java
@@ -1,4 +1,34 @@
 package org.example.betasolutions.login;
 
-public class Login {
-}
+    public class Login {
+        private String username; // mangler vi username i vores ER model?
+        private String password;
+
+        // Default constructor
+        public Login() {}
+
+        // Parameterized constructor
+        public Login(String username, String password) {
+            this.username = username;
+            this.password = password;
+        }
+
+        // Getters and setters
+        public String getUsername() {
+            return username;
+        }
+
+        public void setUsername(String username) {
+            this.username = username;
+        }
+
+        public String getPassword() {
+            return password;
+        }
+
+        public void setPassword(String password) {
+            this.password = password;
+        }
+    }
+
+
--- src/main/java/org/example/betasolutions/login/LoginController.java
+++ src/main/java/org/example/betasolutions/login/LoginController.java
@@ -1,34 +1,41 @@
 package org.example.betasolutions.login;
 
+import ch.qos.logback.core.model.Model;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/login")
 public class LoginController {
-    private LoginService loginService;
+    private final LoginService loginService;
 
     public LoginController(LoginService loginService){
         this.loginService = loginService;
     }
 
     @GetMapping("/login")
-    public String getLogin(){
+    public String getLogin(Model model){
+        model.addText("login", new Login());
         return "loginpage";
     }
 
     @PostMapping("/login")
-    public String postLogin(){
+    public String postLogin(@ModelAttribute Login login){
+        //if the login is authenticated, the login is redirected to their homepage
         return "redirect: /home";
     }
 
     @GetMapping("/login/new")
-    public String getNewProfilePage(){
+    public String getNewProfilePage(Model model){
+        model.addText("newProfile", new Login());
         return "newProfile";
     }
 
     @PostMapping("/login/new")
-    public String postNewLogin(){
+    public String postNewLogin(Model model){
         return "redirect: /login";
     }
 

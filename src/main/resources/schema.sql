49467ac875a5ce6ad37f559ef3abe91eb1dddd44
MH020 <166133291+MH020@users.noreply.github.com>
2024-12-04 09:09:22 +0100

    #17  added test data til joined tables

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,116 +1,104 @@
--- H2 Database Schema: BetaSolutionsDB
-
--- Create schema (H2 automatically creates it with the first table creation)
+-- Create schema (optional for H2)
 -- CREATE SCHEMA IF NOT EXISTS BetaSolutionsDB;
-
--- Use schema (optional for H2)
 -- SET SCHEMA BetaSolutionsDB;
 
--- Table: project
+-- Create Tables
+
 CREATE TABLE IF NOT EXISTS project (
-    project_id INT PRIMARY KEY AUTO_INCREMENT,
-    project_name VARCHAR(100) NOT NULL,
-    project_owner VARCHAR(100) NOT NULL,
-    project_total_hours INT NOT NULL,
-    project_total_days INT NOT NULL,
-    project_total_price DOUBLE NOT NULL,
-    project_deadline DATE NOT NULL,
-    project_start_date DATE NOT NULL,
-    project_active BOOLEAN NOT NULL DEFAULT TRUE
-    );
+                                       project_id INT PRIMARY KEY AUTO_INCREMENT,
+                                       project_name VARCHAR(100) NOT NULL,
+                                       project_owner VARCHAR(100) NOT NULL,
+                                       project_total_hours INT NOT NULL,
+                                       project_total_days INT NOT NULL,
+                                       project_total_price DOUBLE NOT NULL,
+                                       project_deadline DATE NOT NULL,
+                                       project_start_date DATE NOT NULL,
+                                       project_active BOOLEAN NOT NULL DEFAULT TRUE
+);
 
--- Table: sub_project
 CREATE TABLE IF NOT EXISTS sub_project (
-    sub_project_id INT PRIMARY KEY AUTO_INCREMENT,
-    sub_project_name VARCHAR(100) NOT NULL,
-    sub_project_total_hours INT NOT NULL,
-    sub_project_total_days INT NOT NULL,
-    sub_project_total_price DOUBLE NOT NULL,
-    sub_project_deadline DATE NOT NULL,
-    sub_project_start_date DATE NOT NULL,
-    project_id INT NOT NULL,
-    sub_project_active BOOLEAN NOT NULL DEFAULT TRUE,
-    FOREIGN KEY (project_id) REFERENCES project (project_id) ON DELETE CASCADE
-    );
+                                           sub_project_id INT PRIMARY KEY AUTO_INCREMENT,
+                                           sub_project_name VARCHAR(100) NOT NULL,
+                                           sub_project_total_hours INT NOT NULL,
+                                           sub_project_total_days INT NOT NULL,
+                                           sub_project_total_price DOUBLE NOT NULL,
+                                           sub_project_deadline DATE NOT NULL,
+                                           sub_project_start_date DATE NOT NULL,
+                                           project_id INT NOT NULL,
+                                           sub_project_active BOOLEAN NOT NULL DEFAULT TRUE,
+                                           FOREIGN KEY (project_id) REFERENCES project (project_id) ON DELETE CASCADE
+);
 
--- Table: task
 CREATE TABLE IF NOT EXISTS task (
-    task_id INT PRIMARY KEY AUTO_INCREMENT,
-    task_name VARCHAR(100) NOT NULL,
-    task_total_hours INT NOT NULL,
-    task_total_days INT NOT NULL,
-    task_total_price DOUBLE NOT NULL,
-    task_deadline DATE NOT NULL,
-    task_start_date DATE NOT NULL,
-    sub_project_id INT,
-    task_active BOOLEAN NOT NULL DEFAULT TRUE,
-    project_id INT NOT NULL,
-    FOREIGN KEY (sub_project_id) REFERENCES sub_project (sub_project_id) ON DELETE CASCADE,
-    FOREIGN KEY (project_id) REFERENCES project (project_id) ON DELETE CASCADE
-    );
+                                    task_id INT PRIMARY KEY AUTO_INCREMENT,
+                                    task_name VARCHAR(100) NOT NULL,
+                                    task_total_hours INT NOT NULL,
+                                    task_total_days INT NOT NULL,
+                                    task_total_price DOUBLE NOT NULL,
+                                    task_deadline DATE NOT NULL,
+                                    task_start_date DATE NOT NULL,
+                                    sub_project_id INT,
+                                    task_active BOOLEAN NOT NULL DEFAULT TRUE,
+                                    project_id INT NOT NULL,
+                                    FOREIGN KEY (sub_project_id) REFERENCES sub_project (sub_project_id) ON DELETE CASCADE,
+                                    FOREIGN KEY (project_id) REFERENCES project (project_id) ON DELETE CASCADE
+);
 
--- Table: sub_task
 CREATE TABLE IF NOT EXISTS sub_task (
-    sub_task_id INT PRIMARY KEY AUTO_INCREMENT,
-    sub_task_name VARCHAR(100) NOT NULL,
-    sub_task_total_hours INT NOT NULL,
-    sub_task_total_days INT NOT NULL,
-    sub_task_total_price DOUBLE NOT NULL,
-    sub_task_deadline DATE NOT NULL,
-    sub_task_start_date DATE NOT NULL,
-    task_id INT NOT NULL,
-    sub_task_active BOOLEAN NOT NULL DEFAULT TRUE,
-    FOREIGN KEY (task_id) REFERENCES task (task_id) ON DELETE CASCADE
-    );
+                                        sub_task_id INT PRIMARY KEY AUTO_INCREMENT,
+                                        sub_task_name VARCHAR(100) NOT NULL,
+                                        sub_task_total_hours INT NOT NULL,
+                                        sub_task_total_days INT NOT NULL,
+                                        sub_task_total_price DOUBLE NOT NULL,
+                                        sub_task_deadline DATE NOT NULL,
+                                        sub_task_start_date DATE NOT NULL,
+                                        task_id INT NOT NULL,
+                                        sub_task_active BOOLEAN NOT NULL DEFAULT TRUE,
+                                        FOREIGN KEY (task_id) REFERENCES task (task_id) ON DELETE CASCADE
+);
 
--- Table: employee
 CREATE TABLE IF NOT EXISTS employee (
-    employee_id INT PRIMARY KEY AUTO_INCREMENT,
-    employee_name VARCHAR(100) NOT NULL,
-    employee_office VARCHAR(100) NOT NULL,
-    employee_proficiency VARCHAR(100) NOT NULL,
-    employee_salary DOUBLE NOT NULL
-    );
+                                        employee_id INT PRIMARY KEY AUTO_INCREMENT,
+                                        employee_name VARCHAR(100) NOT NULL,
+                                        employee_office VARCHAR(100) NOT NULL,
+                                        employee_proficiency VARCHAR(100) NOT NULL,
+                                        employee_salary DOUBLE NOT NULL
+);
 
--- Table: profile
 CREATE TABLE IF NOT EXISTS profile (
-    login_id INT PRIMARY KEY AUTO_INCREMENT,
-    password VARCHAR(50) NOT NULL,
-    email VARCHAR(50) NOT NULL,
-    employee_id INT NOT NULL,
-    FOREIGN KEY (employee_id) REFERENCES employee (employee_id) ON DELETE CASCADE
-    );
+                                       login_id INT PRIMARY KEY AUTO_INCREMENT,
+                                       password VARCHAR(50) NOT NULL,
+                                       email VARCHAR(50) NOT NULL,
+                                       employee_id INT NOT NULL,
+                                       FOREIGN KEY (employee_id) REFERENCES employee (employee_id) ON DELETE CASCADE
+);
 
--- Table: project_employee
 CREATE TABLE IF NOT EXISTS project_employee (
-    employee_id INT NOT NULL,
-    project_id INT NOT NULL,
-    PRIMARY KEY (employee_id, project_id),
-    FOREIGN KEY (employee_id) REFERENCES employee (employee_id) ON DELETE CASCADE,
-    FOREIGN KEY (project_id) REFERENCES project (project_id) ON DELETE CASCADE
-    );
+                                                employee_id INT NOT NULL,
+                                                project_id INT NOT NULL,
+                                                PRIMARY KEY (employee_id, project_id),
+                                                FOREIGN KEY (employee_id) REFERENCES employee (employee_id) ON DELETE CASCADE,
+                                                FOREIGN KEY (project_id) REFERENCES project (project_id) ON DELETE CASCADE
+);
 
--- Table: project_employee_task
 CREATE TABLE IF NOT EXISTS project_employee_task (
-    employee_id INT NOT NULL,
-    project_id INT NOT NULL,
-    task_id INT NOT NULL,
-    PRIMARY KEY (employee_id, project_id, task_id),
-    FOREIGN KEY (employee_id, project_id) REFERENCES project_employee (employee_id, project_id) ON DELETE CASCADE,
-    FOREIGN KEY (task_id) REFERENCES task (task_id) ON DELETE CASCADE
-    );
+                                                     employee_id INT NOT NULL,
+                                                     project_id INT NOT NULL,
+                                                     task_id INT NOT NULL,
+                                                     PRIMARY KEY (employee_id, project_id, task_id),
+                                                     FOREIGN KEY (employee_id, project_id) REFERENCES project_employee (employee_id, project_id) ON DELETE CASCADE,
+                                                     FOREIGN KEY (task_id) REFERENCES task (task_id) ON DELETE CASCADE
+);
 
--- Table: project_employee_task_subTask
 CREATE TABLE IF NOT EXISTS project_employee_task_subTask (
-    employee_id INT NOT NULL,
-    project_id INT NOT NULL,
-    task_id INT NOT NULL,
-    sub_task_id INT NOT NULL,
-    PRIMARY KEY (employee_id, project_id, task_id, sub_task_id),
-    FOREIGN KEY (employee_id, project_id, task_id) REFERENCES project_employee_task (employee_id, project_id, task_id) ON DELETE CASCADE,
-    FOREIGN KEY (sub_task_id) REFERENCES sub_task (sub_task_id) ON DELETE CASCADE
-    );
-
+                                                             employee_id INT NOT NULL,
+                                                             project_id INT NOT NULL,
+                                                             task_id INT NOT NULL,
+                                                             sub_task_id INT NOT NULL,
+                                                             PRIMARY KEY (employee_id, project_id, task_id, sub_task_id),
+                                                             FOREIGN KEY (employee_id, project_id, task_id) REFERENCES project_employee_task (employee_id, project_id, task_id) ON DELETE CASCADE,
+                                                             FOREIGN KEY (sub_task_id) REFERENCES sub_task (sub_task_id) ON DELETE CASCADE
+);
 
 -- Insert sample data into project table
 INSERT INTO project (project_name, project_owner, project_total_hours, project_total_days, project_total_price, project_deadline, project_start_date, project_active)
@@ -130,17 +118,17 @@ VALUES
     ('Task 1', 100, 5, 1500.0, '2024-12-05', '2024-11-10', 1, 1, 1),
     ('Task 2', 50, 4, 1200.0, '2024-12-20', '2024-11-15', 2, 2, 1);
 
--- Insert sample data into employee table
-INSERT INTO employee (employee_name, employee_office, employee_proficiency, employee_salary)
-VALUES
-    ('Employee 1', 'Office 1', 'Developer', 5000.0),
-    ('Employee 2', 'Office 2', 'Designer', 4000.0);
-
 -- Insert sample data into sub_task table
 INSERT INTO sub_task (sub_task_name, sub_task_total_hours, sub_task_total_days, sub_task_total_price, sub_task_deadline, sub_task_start_date, task_id, sub_task_active)
 VALUES
     ('SubTask 1', 50, 3, 500.0, '2024-12-03', '2024-11-10', 1, 1),
     ('SubTask 2', 25, 2, 300.0, '2024-12-18', '2024-11-15', 2, 1);
+
+-- Insert sample data into employee table
+INSERT INTO employee (employee_name, employee_office, employee_proficiency, employee_salary)
+VALUES
+    ('Employee 1', 'Office 1', 'Developer', 5000.0),
+    ('Employee 2', 'Office 2', 'Designer', 4000.0);
 
 -- Insert sample data into profile table
 INSERT INTO profile (password, email, employee_id)
@@ -148,4 +136,20 @@ VALUES
     ('password1', 'employee1@example.com', 1),
     ('password2', 'employee2@example.com', 2);
 
--- Finished Adding Test Data
+-- Insert sample data into project_employee table
+INSERT INTO project_employee (employee_id, project_id)
+VALUES
+    (1, 1),
+    (2, 2);
+
+-- Insert sample data into project_employee_task table
+INSERT INTO project_employee_task (employee_id, project_id, task_id)
+VALUES
+    (1, 1, 1),
+    (2, 2, 2);
+
+-- Insert sample data into project_employee_task_subTask table
+INSERT INTO project_employee_task_subTask (employee_id, project_id, task_id, sub_task_id)
+VALUES
+    (1, 1, 1, 1),
+    (2, 2, 2, 2);

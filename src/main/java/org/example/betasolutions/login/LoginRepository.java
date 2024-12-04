9f5619369562f54dfdf7813232036dd6baf895f7
carobraaa <166133374+carobraaa@users.noreply.github.com>
2024-12-04 10:44:24 +0100

    ER model sat på

--- src/main/java/org/example/betasolutions/login/LoginRepository.java
+++ src/main/java/org/example/betasolutions/login/LoginRepository.java
@@ -2,7 +2,30 @@ package org.example.betasolutions.login;
 
 import org.springframework.stereotype.Repository;
 
+import java.sql.Connection;
+
 @Repository
 public class LoginRepository {
+
+    private final Connection conn;
+
+    public LoginRepository(ConnectionManager connectionManager) {
+        this.conn = connectionManager.getConnection();
+
+    }
+    public int saveUser(Login login) {
+        String sql = "INSERT INTO login (password, email) VALUES (?, ?)";
+try(PreparedStatement preparedStatement = conn.prepareStatement(sql, PreparedStatement.RETURN_GENERATED_KEYS)) {
+prepraredStatement.setString(1, login.getPassword());
+preparedStatement.setString(2, login.getEmail());
+preparedStatement.executeUpdate();
+ResultSet resultSet = preparedStatement.getGeneratedKeys();
+if(resultSet.next()) {
+
+}
+
+
+    }
+
 
 }

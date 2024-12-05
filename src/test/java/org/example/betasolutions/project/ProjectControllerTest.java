9f52bbe3378f4d23bada90b1835378be6f88a3e4
signelystrup <166133407+signelystrup@users.noreply.github.com>
2024-12-05 12:16:49 +0100

    #11 test klasser sat op

---
+++ src/test/java/org/example/betasolutions/project/ProjectControllerTest.java
@@ -0,0 +1,45 @@
+package org.example.betasolutions.project;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProjectControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+
+    @Test
+    void getHome() {
+        assertTrue(false);
+    }
+
+    @Test
+    void createNewProject() {
+    }
+
+    @Test
+    void getProject() {
+        assertTrue(false);
+    }
+
+    @Test
+    void deleteProject() {
+    }
+
+    @Test
+    void editProject() {
+    }
+
+    @Test
+    void getProjectBudget() {
+    }
+}

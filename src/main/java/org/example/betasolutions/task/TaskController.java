5434a8ca5ac1148147d8b540571a39dacdb57c25
signelystrup <166133407+signelystrup@users.noreply.github.com>
2024-12-05 14:01:53 +0100

    #33 okay knappen virker ikke helt.

--- src/main/java/org/example/betasolutions/task/TaskController.java
+++ src/main/java/org/example/betasolutions/task/TaskController.java
@@ -2,10 +2,7 @@ package org.example.betasolutions.task;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class TaskController {

a2170d2961dc4b2d0aa0010126588394b580ff3a
MH020 <166133291+MH020@users.noreply.github.com>
2024-12-05 08:58:09 +0100

    #27 controller

--- src/main/java/org/example/betasolutions/subProject/SubProjectController.java
+++ src/main/java/org/example/betasolutions/subProject/SubProjectController.java
@@ -1,6 +1,8 @@
 package org.example.betasolutions.subProject;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -9,6 +11,14 @@ public class SubProjectController {
 
     public SubProjectController(SubProjectService subProjectService){
         this.subProjectService = subProjectService;
+    }
+
+    @GetMapping("/project/subproject")
+    public String getSubProjects(Model model){
+        model.addAttribute("subproject", new SubProject());
+        model.addAttribute("subproject_overview", subProjectService.
+
+        return "projectpage";
     }
 
     @PostMapping("/project/subproject/edit")

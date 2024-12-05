ed78ea3158c795a4f4ba988b729e1f3daa25ef98
MH020 <166133291+MH020@users.noreply.github.com>
2024-12-05 08:58:56 +0100

    #27 service

--- src/main/java/org/example/betasolutions/subProject/SubProjectService.java
+++ src/main/java/org/example/betasolutions/subProject/SubProjectService.java
@@ -9,4 +9,13 @@ public class SubProjectService {
     public SubProjectService(SubProjectRepository subProjectRepository){
         this.subProjectRepository = subProjectRepository;
     }
+    public void insertIntoSubProject(SubProject subProject){
+        subProjectRepository.insertIntoSubProject(subProject);
+    }
+    public void readAllSubProjects(int projectID){
+        subProjectRepository.readAllSubProjects(projectID);
+    }
+    public void readSubProjectByID(int subProjectID){
+        subProjectRepository.readSubProjectByID(subProjectID);
+    }
 }

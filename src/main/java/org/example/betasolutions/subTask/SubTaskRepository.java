e61d20e69e1917a00b9446ca4f08049dbd1abb72
MH020 <166133291+MH020@users.noreply.github.com>
2024-12-07 10:56:31 +0100

    #25 opsætning af subtask

--- src/main/java/org/example/betasolutions/subTask/SubTaskRepository.java
+++ src/main/java/org/example/betasolutions/subTask/SubTaskRepository.java
@@ -4,11 +4,23 @@ import org.example.betasolutions.ConnectionManager;
 import org.example.betasolutions.PSSTSuperclass;
 import org.springframework.stereotype.Repository;
 
+import java.sql.PreparedStatement;
+
 @Repository
 public class SubTaskRepository extends PSSTSuperclass {
 
     public SubTaskRepository(ConnectionManager connectionManager) {
         super(connectionManager);
+    }
+    public void addSubTaskForTask(SubTask subTask){
+        String sql = "insert into sub_task (sub_task_name, sub_task_total_hours,sub_task_total_days,sub_task_total_price,sub_task_deadline,sub_task_start_date,task_id) values(?,?,?,?,?,?,?)";
+        PreparedStatement preparedStatement = super.insertAssignmentIntoTable(subTask,sql);
+        try{
+            preparedStatement.setInt(7,subTask.getTaskID());
+            preparedStatement.executeUpdate();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 
 }

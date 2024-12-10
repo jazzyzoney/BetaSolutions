6344cb15de1daada9bc8713fccde037f88638d7d
jazzyzoney <jaqtash22@gmail.com>
2024-12-10 12:54:08 +0100

    #13  test klasse sat op

---
+++ src/test/java/org/example/betasolutions/employee/EmployeeRepositoryTest.java
@@ -0,0 +1,36 @@
+package org.example.betasolutions.employee;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EmployeeRepositoryTest {
+
+    @Test
+    void createNewEmployee() {
+    }
+
+    @Test
+    void getAllEmployees() {
+    }
+
+    @Test
+    void getAllEmployeesForProject() {
+    }
+
+    @Test
+    void getAllEmployeesNotOnProject() {
+    }
+
+    @Test
+    void addExistingEmployeeToProject() {
+    }
+
+    @Test
+    void addExistingEmployeeToTask() {
+    }
+
+    @Test
+    void addExistingEmployeeToSubTask() {
+    }
+}

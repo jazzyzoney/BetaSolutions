bc655483803acc3a6194b2a16b6c7269eeb096b6
carobraaa <166133374+carobraaa@users.noreply.github.com>
2024-12-10 14:53:36 +0100

    Login funktion

--- src/main/java/org/example/betasolutions/login/LoginService.java
+++ src/main/java/org/example/betasolutions/login/LoginService.java
@@ -1,12 +1,15 @@
 package org.example.betasolutions.login;
+import org.example.betasolutions.employee.EmployeeRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class LoginService {
     private final LoginRepository loginRepository;
+    private  final EmployeeRepository employeeRepository;
 
-    public LoginService(LoginRepository loginRepository){
+    public LoginService(LoginRepository loginRepository, EmployeeRepository employeeRepository) {
         this.loginRepository = loginRepository;
+        this.employeeRepository = employeeRepository;
     }
     public boolean validateLogin(Login login) {
         Login storedLogin = loginRepository.findByEmail(login.getEmail());
@@ -14,11 +17,10 @@ public class LoginService {
     }
 
     public void createLogin(Login login) {
-
         loginRepository.createLogin(login);
     }
 
-    public int findEmployeeByEmail(String email) {
-        return loginRepository.findEmployeeByEmail(email);
-    }
+    //public int findEmployeeByEmail(String email) {
+        //return employeeRepository.findEmployeeByEmail(email);
+  //  }
 }

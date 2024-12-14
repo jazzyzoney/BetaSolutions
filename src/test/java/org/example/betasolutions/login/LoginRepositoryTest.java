687dee338f8b752940ece4b2db25a58c3cd7034a
MH020 <166133291+MH020@users.noreply.github.com>
2024-12-14 20:18:26 +0100

    tests

---
+++ src/test/java/org/example/betasolutions/login/LoginRepositoryTest.java
@@ -0,0 +1,16 @@
+package org.example.betasolutions.login;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LoginRepositoryTest {
+
+    @Test
+    void createLogin() {
+    }
+
+    @Test
+    void vaifyLogin() {
+    }
+}

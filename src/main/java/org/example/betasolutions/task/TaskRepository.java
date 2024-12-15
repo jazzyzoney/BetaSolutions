2b24dfc0d5a45761e1ea4025d173dfcb4d3890eb
MH020 <166133291+MH020@users.noreply.github.com>
2024-12-15 19:30:59 +0100

    ændret til at vi gør brug a subtask id istedet

--- src/main/java/org/example/betasolutions/task/TaskRepository.java
+++ src/main/java/org/example/betasolutions/task/TaskRepository.java
@@ -191,7 +191,7 @@ public class TaskRepository extends PSSTSuperclass {
 
         for (ModelInterface modelInterface : allSubTasks){
             SubTask subTask = (SubTask) modelInterface; //typecasting.
-            subTask.setTaskID(getTableIntByInt("sub_task", "task_id", "sub_task_id", task.getID()));//set task id for subtask.
+            subTask.setTaskID(getTableIntByInt("sub_task", "task_id", "sub_task_id", subTask.getID()));//set task id for subtask.
 
             if (subTask.getTaskID() == task.getID()){
                 totalHoursForTask += subTask.getHours(); //add subtask-specific hours to total.
